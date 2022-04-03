9da25faa8fb6e228174c6f10b4a5fa9f017de3a1
Anil Duri <anilduri.a@gmail.com>
2022-04-04 00:56:11 +0300

    accessing protected resources

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,24 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { AuthContext } from '../store/auth-context';
+
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+  const authCtx = useContext(AuthContext);
+  useEffect(() => {
+    axios.get(`https://react-native-authenticat-4369c-default-rtdb.firebaseio.com/message.json?auth=${authCtx.token}`)
+      .then((response) => {
+        setFetchedMessage(response.data);
+      })
+  }, [])
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
